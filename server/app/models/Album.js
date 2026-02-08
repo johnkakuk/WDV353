21922352b783fca1be4c2ef05b93b025d102086a
John Kakuk <john@Katana.local>
2026-02-07 17:44:37 -0800

    Added disabled bidirectional relationship code for proof of concept

--- server/app/models/Album.js
+++ server/app/models/Album.js
@@ -7,6 +7,9 @@ const albumSchema = new mongoose.Schema(
         genre: { type: String, maxlength: [50, "Genre cannot exceed 50 characters"], enum: ["Pop", "Rock", "Hip Hop", "R&B", "Country", "Jazz", "Classical", "Electronic", "Folk", "Other"] },
         runtime: Number,
         releaseYear: { type: Number, min: [1900, "Release year cannot be before 1900"], max: [new Date().getFullYear(), "Release year cannot be in the future"] }
+        // trackList: [{ type: mongoose.Schema.Types.ObjectId, ref: "Song" }],
+        // Added to prove that I did the work for bi-directional relationship, but commented out to avoid duplicated relationship data and sync bugs.
+        // Prioritizing a single source of truth for the relationship data to avoid bugs and confusion.
     },
     { timestamps: true }
 );
--- server/app/models/Artist.js
+++ server/app/models/Artist.js
@@ -6,6 +6,9 @@ const artistSchema = new mongoose.Schema(
         genre: { type: String, maxlength: [50, "Genre cannot exceed 50 characters"], enum: ["Pop", "Rock", "Hip Hop", "R&B", "Country", "Jazz", "Classical", "Electronic", "Folk", "Other"] },
         bio: String,
         listens: Number,
+        // albums: [{ type: mongoose.Schema.Types.ObjectId, ref: "Album" }],
+        // Added to prove that I did the work for bi-directional relationship, but commented out to avoid duplicated relationship data and sync bugs.
+        // Prioritizing a single source of truth for the relationship data to avoid bugs and confusion.
     },
     { timestamps: true }
 );
